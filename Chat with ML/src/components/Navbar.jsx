a2e2bbd81ead97b0bc2e92eb46681e114685245d
v-Shivaprasad <velagadaa@gmail.com>
2024-07-01 16:23:03 +0530

    s - minor frontend adjustmnets

--- Chat with ML/src/components/Navbar.jsx
+++ Chat with ML/src/components/Navbar.jsx
@@ -98,40 +98,43 @@ const Navbar = () => {
   ];
 
   return (
-    <div className='shadow-md w-full fixed top-0 left-0'>
-      <div className='md:flex items-center justify-between bg-white dark:bg-primary-97 py-4 md:px-10 px-4'>
-        <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-black dark:text-white'>
+    <div className="shadow-md w-full fixed top-0 left-0">
+      <div className="md:flex items-center justify-between bg-white dark:bg-primary-97 py-4 md:px-10 px-4">
+        <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-black dark:text-white">
           <MyComponent />
-          <span className='text-3xl text-indigo-600 mr-1 pt-2'>
-            <GiFlyingTarget color='orange' />
+          <span className="text-3xl text-indigo-600 mr-1 pt-2">
+            <GiFlyingTarget color="orange" />
           </span>
           <div>Chat with Ml</div>
-          <div className='absolute right-4'>
+          <div className="absolute right-4">
             <DropMenu MenuItems={menuItems} />
           </div>
         </div>
 
         <div
           onClick={() => setOpen(!open)}
-          className='text-3xl absolute right-16 top-6 cursor-pointer md:hidden'
+          className="text-3xl absolute right-16 top-6 cursor-pointer md:hidden"
         >
           {open ? (
-            <RxCross1 className='dark-btn' color={`${dark ? 'white' : 'black'}`} />
+            <RxCross1
+              className="dark-btn"
+              color={`${dark ? "white" : "black"}`}
+            />
           ) : (
-            <GiHamburgerMenu className='dark-btn' />
+            <GiHamburgerMenu className="dark-btn" />
           )}
         </div>
 
         <ul
-          className={`dark:bg-primary-97 md:flex md:items-center md:pb-0 pb-4 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-            open ? 'top-10 ' : 'top-[-490px]'
+          className={`dark:bg-primary-97 md:flex md:items-center md:pb-0 pb-4 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 ${
+            open ? "top-10 " : "top-[-490px]"
           }`}
         >
           {Links.map((link) => (
-            <li key={link.name} className='md:ml-6 text-xl md:my-0 my-7 mr-8'>
+            <li key={link.name} className="md:ml-6 text-xl md:my-0 my-7 mr-8">
               <a
                 href={link.link}
-                className='text-gray-800 hover:text-gray-400 duration-500 dark:text-white dark:hover:text-blue-500'
+                className="text-gray-800 hover:text-gray-400  dark:text-white dark:hover:text-blue-500 "
               >
                 {link.name}
               </a>
