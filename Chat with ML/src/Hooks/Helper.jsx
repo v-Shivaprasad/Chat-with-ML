3a9fa06027737a1289c4c7c08de9e189fc2df949
udaykumarcoder <methreudaykumar@gmail.com>
2024-07-25 13:55:37 +0530

    ukm

--- Chat with ML/src/Hooks/Helper.jsx
+++ Chat with ML/src/Hooks/Helper.jsx
@@ -58,7 +58,7 @@ export const loginwithemailandPassword = async (FormData) => {
 export const getLLMResponse = async (text) => {
   try {
     const response = await fetch(
-      "https://5a24-34-142-135-186.ngrok-free.app/predict",
+      "https://aae8-34-16-134-227.ngrok-free.app/predict",
       {
         method: "POST",
         headers: {
